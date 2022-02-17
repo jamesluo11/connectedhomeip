dd8ca35482c703c7689173d1f18e41d52c69e2cd
yongjia.fang <yongjia.fang@bekencorp.com>
2022-02-17 10:34:30 +0800

    network commission refactor

--- src/platform/Beken/BekenConfig.h
+++ src/platform/Beken/BekenConfig.h
@@ -63,12 +63,13 @@ public:
     static const Key kConfigKey_SetupDiscriminator;
     static const Key kConfigKey_RegulatoryLocation;
     static const Key kConfigKey_CountryCode;
-    static const Key kConfigKey_ActiveLocale;
     static const Key kConfigKey_Breadcrumb;
-    static const Key kConfigKey_HourFormat;
-    static const Key kConfigKey_CalendarType;
+    static const Key kConfigKey_Spake2pIterationCount;
+    static const Key kConfigKey_Spake2pSalt;
+    static const Key kConfigKey_Spake2pVerifier;
 
     // Counter keys
+    static const Key kConfigKey_UniqueId;
     static const Key kCounterKey_RebootCount;
     static const Key kCounterKey_UpTime;
     static const Key kCounterKey_TotalOperationalHours;
